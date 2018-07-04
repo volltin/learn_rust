9322f8b81687bf1adb9259680ee3c1aabc40b359
hejiyan <hejiyan@mail.ustc.edu.cn>
2018-07-04 22:40:39 +0800

    rm

--- ownership/target/debug/deps/ownership-9e320f9559269184.d
+++
@@ -1,5 +0,0 @@
-/Users/hejiyan/Projects/learn_rust/ownership/target/debug/deps/ownership-9e320f9559269184: src/main.rs
-
-/Users/hejiyan/Projects/learn_rust/ownership/target/debug/deps/ownership-9e320f9559269184.d: src/main.rs
-
-src/main.rs:
--- ownership/target/debug/ownership.d
+++
@@ -1,1 +0,0 @@
-/Users/hejiyan/Projects/learn_rust/ownership/target/debug/ownership.dSYM: /Users/hejiyan/Projects/learn_rust/ownership/src/main.rs
