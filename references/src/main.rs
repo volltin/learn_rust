9c8cfc031bc6dcf3f43213360c078d759fb6261a
hejiyan <hejiyan@mail.ustc.edu.cn>
2018-07-05 14:00:53 +0800

    add ref

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    // ref
+    let s = String::from("hello");
+
+    let l = calc_len(&s);
+
+    println!("s len: {} {}", s, l);
+
+    // mut ref
+    let mut s = String::from("233");
+    change(&mut s);
+    println!("s: {}", s);
+
+    // multiple ref
+    let s = String::from("hello! ");
+
+    let ref1 = &s;
+    let ref2 = &s;
+    println!("s ref1 ref2: {} {} {}", s, ref1, ref2);
+
+    // multiple mut ref
+    let mut s = String::from("hello! ");
+
+    let ref1 = &mut s;
+    //let ref2 = &mut s; // second mutable borrow occurs here
+    //println!("s ref1 ref2: {} {} {}", s, ref1, ref2);
+}
+
+fn calc_len(s: &String) -> usize {
+    //s.push_str(", world"); //cannot borrow as mutable
+    println!("(calc_len) s: {}", s);
+    s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(", world");
+    println!("(change) s: {}", s);
 }
